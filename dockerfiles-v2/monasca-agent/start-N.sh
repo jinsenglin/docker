f1f9fb7b5367d0d99e0d3f096c0a3c8e427038e0
Jim Lin <jimlin@cloudcube.com.tw>
2017-02-21 02:32:50 -0500

    update helper scripts

--- dockerfiles-v2/monasca-agent/start-N.sh
+++ dockerfiles-v2/monasca-agent/start-N.sh
@@ -1,4 +1,6 @@
-for n in {1..2}
+N=2
+for n in $(seq 1 $N)
 do
-    docker run -d -e "u=monasca" -e "p=nomoresecret" -e "pn=service" -e "ku=http://192.168.120.226:35357/v3" --name "monasca-agent-$n" monasca-agent
+    echo starting $n / $N ...
+    docker run -d -e "u=monasca" -e "p=nomoresecret" -e "pn=service" -e "ku=http://192.168.120.226:35357/v3" --name "monasca-agent-$n" -h "monasca-agent-$n" monasca-agent
 done
--- dockerfiles-v2/monasca-agent/stop-and-rm-N.sh
+++ dockerfiles-v2/monasca-agent/stop-and-rm-N.sh
@@ -1,5 +1,7 @@
-for n in {1..2}
+N=2
+for n in $(seq 1 $N)
 do
+    echo stopping $n / $N ...
     docker stop "monasca-agent-$n"
     docker rm "monasca-agent-$n"
 done
