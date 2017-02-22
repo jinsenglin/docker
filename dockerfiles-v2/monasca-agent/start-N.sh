327054608895848dfcb975e929b075bcc20a4a67
Jim Lin <jimlin@cloudcube.com.tw>
2017-02-22 01:37:28 -0500

    update start-N.sh

--- dockerfiles-v2/monasca-agent/start-N.sh
+++ dockerfiles-v2/monasca-agent/start-N.sh
@@ -1,6 +1,15 @@
-N=2
+# 6: startup one container every 6 seconds
+# => 10 agents every 1 minutes (assume agent startup time 0)
+# => 600 agents needs 60 minutes, i.e., 1h
+# => 1000 agents needs 100 minutes, i.e., 1h40m
+
+S=6
+N=600
 for n in $(seq 1 $N)
 do
     echo starting $n / $N ...
     docker run -d -e "u=monasca" -e "p=nomoresecret" -e "pn=service" -e "ku=http://192.168.120.226:35357/v3" --name "monasca-agent-$n" -h "monasca-agent-$n" monasca-agent
+
+    echo waiting $S seconds to start next one
+    sleep $S
 done
