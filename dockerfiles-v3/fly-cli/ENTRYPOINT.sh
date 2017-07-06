fca00e21c6940dfd6b8e9487deba70855d02a492
Jim Lin <jimlintw922@gmail.com>
2017-07-06 22:02:51 +0800

    update fly-cli

--- dockerfiles-v3/fly-cli/ENTRYPOINT.sh
+++ dockerfiles-v3/fly-cli/ENTRYPOINT.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+VAR_OPENRC=${OPENRC:-openrc.sh}
+VAR_DATA=/data
+
+source $VAR_DATA/$VAR_OPENRC
 exec fly $@
---
+++ dockerfiles-v3/fly-cli/data/another-openrc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+fly -t ccc l -c http://192.168.100.4:8080 --team-name=main --username=admin --password=admin
